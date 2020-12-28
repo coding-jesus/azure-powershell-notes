df0b44a4f8f2b7244afc90b2d1e4ed06f756db4c
Sayid <6471104+coding-jesus@users.noreply.github.com>
2020-12-28 14:04:48 -0600

    Added connection via application secret

--- Azure-Connection.ps1
+++ Azure-Connection.ps1
@@ -4,10 +4,16 @@
 # Context persistence: https://docs.microsoft.com/en-us/powershell/azure/context-persistence
 Connect-AzAccount -Environment 'AzureCloud'
 
+# Connection via application secret.
+$azureApplicationId = Get-Content ./AzAppID.txt -Raw
+$azureTenantId = Get-Content ./AzTenantId.txt -Raw
+$azureSecret = Get-Content ./AzSecret.txt -Raw
+$azureSecretSecured = ConvertTo-SecureString $azureSecret -AsPlainText -Force
+$psCred = New-Object System.Management.Automation.PSCredential($azureApplicationId , $azureSecretSecured)
+Connect-AzAccount -Credential $psCred -TenantId $azureTenantId  -ServicePrincipal
+
 # Az Context files are located at ~\.Azure folder.
 
 Clear-AzContext # Removes any previously stored sessions.
-Get-AzContext # Shows the subscriptions you are currently running under
-Get-AzSubscription # Lists all available subscriptions.
-
+Get-AzContext # Shows the subscriptions currently active.
 Set-AzContext 'Azure Free Trial' # Change current active subscription.
